7673f16d1fbfaa9ec0e54c6378b06de56d2d3df4
Scott Vitale <svvitale@gmail.com>
2017-08-09 16:55:01 -0600

    Update build script to be Fastboot 1.0 compatible.

--- index.js
+++ index.js
@@ -6,45 +6,38 @@ var merge = require('lodash/merge');
 var replace = require('broccoli-string-replace');
 var mergeTrees = require('broccoli-merge-trees');
 var path = require('path');
+var map = require('broccoli-stew').map;
 
 module.exports = {
   name: 'ember-cli-rollbar',
   included: function(app) {
-    var config = this.project.config(this.app.env).rollbar || {};
-    var defaultEnabled = this.app.env !== 'development' && this.app.env !== 'test';
-    var enabled = config.enabled == null ? defaultEnabled : config.enabled;
-    if (process.env.EMBER_CLI_FASTBOOT !== 'true') {
-      app.import('vendor/rollbar.js', {
-        prepend: true
-      });
-      app.import('vendor/rollbar-module.js');
-    }
+    app.import('vendor/rollbar-module.js');
   },
   treeForVendor: function(vt) {
     var vendorTree = this._super.treeForVendor(vt);
 
-    if (process.env.EMBER_CLI_FASTBOOT !== 'true') {
-      var config = this.project.config(this.app.env).rollbar || {};
-      config = merge({
-        enabled: this.app.env !== 'development' && this.app.env !== 'test',
-        captureUncaught: true,
-        payload: {
-          environment: this.app.env
-        }
-      }, config);
-      var clientTree = replace(path.join(__dirname, 'client'), {
-        files: ['rollbar.js'],
-        pattern: {
-          match: /ROLLBAR_CONFIG/g,
-          replacement: JSON.stringify(config)
-        }
-      });
+    var config = this.project.config(this.app.env).rollbar || {};
 
-      return mergeTrees([vendorTree, clientTree], {
-        overwrite: true
-      });
-    } else {
-      return vendorTree;
-    }
+    config = merge({
+      enabled: this.app.env !== 'development' && this.app.env !== 'test',
+      captureUncaught: true,
+      payload: {
+        environment: this.app.env
+      }
+    }, config);
+
+    var clientTree = replace(path.join(__dirname, 'client'), {
+      files: ['rollbar.js'],
+      pattern: {
+        match: /ROLLBAR_CONFIG/g,
+        replacement: JSON.stringify(config)
+      }
+    });
+
+    var clientTree = map(vendorTree, (content) => `if (typeof FastBoot === 'undefined') { ${content} }`);
+
+    return mergeTrees([vendorTree, clientTree], {
+      overwrite: true
+    });
   }
 };
